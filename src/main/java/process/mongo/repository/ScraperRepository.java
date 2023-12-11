82a5fb4fd6297340511008e2a9b60a5da2d3850d
Nabeel Ahmed <nabeel.amd93@gmail.com>
2023-12-11 10:14:19 +0300

    Query Modifier

--- src/main/java/process/mongo/repository/ScraperRepository.java
+++
@@ -1,9 +0,0 @@
-package process.mongo.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import process.mongo.documents.Scraper;
-
-@Repository
-public interface ScraperRepository extends MongoRepository<Scraper, String> {
-}
