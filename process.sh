7c24221df3905cea2237d64e75731e4561efa28a
Nabeel Ahmed <nabeel.amd93@gmail.com>
2024-02-21 23:28:31 +0300

    # Step 5: Kubernetes Pending
    # Step 5: Kubernetes Pending

--- process.sh
+++ process.sh
@@ -4,23 +4,24 @@ echo "User: Nabeel Ahmed"
 echo "Email: nabeel.amd93@gmail.com"
 echo "===============================================================";
 
-# project local build
-echo "Project Clean Start"
-# Step 2: Running Maven clean to remove target directory
+# Step 1: Running Maven clean to remove target directory...
 echo "Step 1: Running Maven clean to remove target directory..."
 mvn clean
-echo "===============================================================";
-# mvn install
+
+# Step 2: Running Maven install to compile and package the project...
 echo "Step 2: Running Maven install to compile and package the project..."
 mvn install
 
-# running the docker image detail
-current_datetime=$(date +%Y-%m-%d_%H-%M-%S)
-container_name="process-${current_datetime}"
-echo "======================$container_name==========================";
+# Step 3: Build Docker Image...
 echo "Step 3: Build Docker Image..."
-docker build -t process .
-echo "===============================================================";
-echo "Step 4: Run Docker container..."
-docker run -d --name "$container_name" process
-echo "Build And Run Process Complete."
+docker build -t ballistic/process:latest .
+
+# Step 4: Docker Hub Login
+echo "Step 4: Docker Hub Login"
+docker login -u ballistic -p dckr_pat_tbeLFqXGfCqWXDUQE_xeqTFl0Lw
+
+# Step 5: Docker Image Upload
+echo "Step 5: Docker Image Upload"
+docker push ballistic/process-v2:latest
+
+# Step 6: Run Kubernat
